3f9968b0061867e7766d62ef0662a27a16aa5681
JoshNapier <ST10291238@vcconnect.edu.za>
2024-04-15 16:00:23 +0200

    Added input methods

--- RecipeApplication/Program.cs
+++ RecipeApplication/Program.cs
@@ -10,7 +10,16 @@ namespace RecipeApplication
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Recipe Creator Program");
+
+            Console.Write("What is the name of your recipe?");
+            string name = Console.ReadLine();
+
+            Console.Write("How many ingredients?");
+            int numIngredients = int.Parse(Console.ReadLine());
+
+            Console.Write("How many steps of instructions?");
+            int numSteps = int.Parse(Console.ReadLine());
         }
     }
 }
